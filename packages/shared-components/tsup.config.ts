4f92973a03358ba3c43581a515ac1057c244ff41
hanlee <me@hanlee.co>
2021-05-28 15:31:21 +0900

    chore: switch to esbuild-plugin-raw

--- packages/shared-components/tsup.config.ts
+++ packages/shared-components/tsup.config.ts
@@ -1,32 +1,10 @@
-import fs from 'fs'
-import path from 'path'
 import { defineConfig } from 'tsup'
-import { Plugin } from 'esbuild'
-
-function rawPlugin(): Plugin {
-  return {
-    name: 'raw',
-    setup(build) {
-      build.onResolve({ filter: /\?raw$/ }, (args) => {
-        return {
-          path: path.isAbsolute(args.path) ? args.path : path.join(args.resolveDir, args.path),
-          namespace: 'raw-loader',
-        }
-      })
-      build.onLoad({ filter: /\?raw$/, namespace: 'raw-loader' }, async(args) => {
-        return {
-          contents: await fs.promises.readFile(args.path.replace(/\?raw$/, '')),
-          loader: 'text',
-        }
-      })
-    },
-  }
-}
+import RawPlugin from 'esbuild-plugin-raw'
 
 export default defineConfig({
   entryPoints: ['src/index.ts'],
   format: ['cjs', 'esm'],
   dts: true,
-  esbuildPlugins: [rawPlugin()],
+  esbuildPlugins: [RawPlugin()],
   clean: true,
 })
